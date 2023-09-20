85bcdec17238aa45274069ecc12c03965ba7d121
PandaLoic <panel.loic@gmail.com>
2023-09-20 12:03:41 +0200

    suppr com

--- Models/Role.cs
+++ Models/Role.cs
@@ -5,6 +5,5 @@
         public string ID { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
         public string Description { get; set; }
-        // petit commentaire de test pour github
     }
 }
